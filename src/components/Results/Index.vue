a1aa82fcaa5b71b0a270bd1e68e39fd7f74bb0e4
dreblen <tannerjotblad@gmail.com>
2026-01-08 19:20:48 -0600

    Results: Layout Improvements
    - Moved grade details into an expansion panel with more layout controls to make them easier to read.
    - Moved schedule table into a container for consistency.

--- src/components/Results/Index.vue
+++ src/components/Results/Index.vue
@@ -47,30 +47,58 @@ const uniqueShiftNames = computed(() => {
                 No schedule currently selected.
             </template>
             <template v-else>
-                <p>Overall Grade: {{ (schedule as Schedule).grade?.overall }}</p>
-                <p v-for="component of (schedule as Schedule).grade?.components" :key="component.name">
-                    Component "{{ component.name }}" (Weight = {{ component.weight }}): {{ component.value }}
-                </p>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Event</th>
-                            <th v-for="name in uniqueShiftNames" :key="name">
-                                {{ name }}
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="event in (schedule as Schedule).events" :key="event.id">
-                            <td>{{ event.name }}</td>
-                            <td v-for="name in uniqueShiftNames" :key="name">
-                                <p v-for="slot in event.shifts.find((s) => s.name === name)?.slots" :key="slot.id">
-                                    {{ slot.name }}: {{ setup.workers.find((w) => w.id === slot.workerId)?.name || 'N/A' }}
-                                </p>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <v-container>
+                    <v-row>
+                        <v-col>
+                            <v-expansion-panels>
+                                <v-expansion-panel>
+                                    <v-expansion-panel-title>
+                                        <v-chip :color="getColorForGrade((schedule as Schedule).grade?.overall)">
+                                            {{ (schedule as Schedule).grade?.overall }}
+                                        </v-chip>
+                                        Grade Details...
+                                    </v-expansion-panel-title>
+                                    <v-expansion-panel-text>
+                                        <v-row v-for="component of (schedule as Schedule).grade?.components" :key="component.name">
+                                            <v-col cols="4" sm="2" lg="1">
+                                                <v-chip density="compact" :color="getColorForGrade(component.value)">
+                                                    {{ component.value }}
+                                                </v-chip>
+                                            </v-col>
+                                            <v-col>
+                                                {{ component.name }} (Weight = {{ component.weight }}%)
+                                            </v-col>
+                                        </v-row>
+                                    </v-expansion-panel-text>
+                                </v-expansion-panel>
+                            </v-expansion-panels>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Event</th>
+                                        <th v-for="name in uniqueShiftNames" :key="name">
+                                            {{ name }}
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="event in (schedule as Schedule).events" :key="event.id">
+                                        <td>{{ event.name }}</td>
+                                        <td v-for="name in uniqueShiftNames" :key="name">
+                                            <p v-for="slot in event.shifts.find((s) => s.name === name)?.slots" :key="slot.id">
+                                                {{ slot.name }}: {{ setup.workers.find((w) => w.id === slot.workerId)?.name || 'N/A' }}
+                                            </p>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </v-col>
+                    </v-row>
+                </v-container>
             </template>
         </template>
         <template #append="{ item }">
