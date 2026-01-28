9de5ff46bccfed66cacba5571255f1259fe7f1e2
dreblen <tannerjotblad@gmail.com>
2026-01-28 07:02:37 -0600

    Home Page
    Added home route stub to redirect on initial page load.

---
+++ src/components/Home/Index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// This is a stub to redirect any base requests
+import { useRouter } from 'vue-router'
+const router = useRouter()
+router.push('/setup')
+</script>
+
+<template>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Home from '@/components/Home/Index.vue'
 import Setup from '@/components/Setup/Index.vue'
 import Parameters from '@/components/Parameters/Index.vue'
 import Results from '@/components/Results/Index.vue'
@@ -7,6 +8,7 @@ import Results from '@/components/Results/Index.vue'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        { path: '/', component: Home },
         { path: '/setup', component: Setup },
         { path: '/parameters', component: Parameters },
         { path: '/results', component: Results },
